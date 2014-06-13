aff8c9acccafefd2f879ed172ba1d5e9cd6a4d91
pgt7 <p_g_t@hotmail.com>
2014-06-13 01:04:26 -0300

    Se crea la app de consola que consulta informacion de los  usuarios

---
+++ TP2/Business.Entities/BusinessEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Entities
+{
+    public class BusinessEntity
+    {
+        private States _State;
+        private int _Id;
+
+        public BusinessEntity()
+        {
+            this.State = States.New;
+        }
+       
+        public States State {
+            get { return _State; }
+            set { this._State = value;}
+        }
+        public int Id
+        {
+            get { return _Id; }
+            set { this._Id = value; }
+        }
+
+        
+        public enum States { 
+            Deleted,
+            New,
+            Modified,
+            Unmodified
+        }
+    }
+}
