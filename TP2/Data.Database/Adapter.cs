b03f4f9c236ac931715f3cf740eb8696dacf808b
pgt7 <p_g_t@hotmail.com>
2014-10-06 22:42:11 -0300

    Update Adapter.cs

--- TP2/Data.Database/Adapter.cs
+++ TP2/Data.Database/Adapter.cs
@@ -2,21 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Data.Database
 {
     public class Adapter
     {
-        //private SqlConnection sqlConnection = new SqlConnection("ConnectionString;");
+
+        //Clave por defecto a utlizar para la cadena de conexion
+        const string consKeyDefaultCnnString = "ConnStringLocal";
+
+        SqlConnection sqlConn;
+
+        
+
 
         protected void OpenConnection()
         {
-            throw new Exception("Metodo no implementado");
+            SqlConnection sqlConn = new SqlConnection(ConfigurationManager.ConnectionStrings[consKeyDefaultCnnString].ConnectionString);
+
+            //throw new Exception("Metodo no implementado");
         }
 
         protected void CloseConnection()
         {
-            throw new Exception("Metodo no implementado");
+
+            sqlConn.Close();
+            sqlConn = null;
+            //throw new Exception("Metodo no implementado");
         }
 
         protected SqlDataReader ExecuteReader(String commandText)
