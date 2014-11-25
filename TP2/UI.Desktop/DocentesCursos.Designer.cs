4bf6833ca67c7260ca0d774474c0d34ebe8c2b0b
pgt7 <p_g_t@hotmail.com>
2014-11-24 22:08:26 -0300

    Va el form de asignacion de docentes

---
+++ TP2/UI.Desktop/DocentesCursos.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace UI.Desktop
+{
+    partial class DocentesCursos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlUsuarios = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvDocentes = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.cmbDocentes = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quitar = new System.Windows.Forms.DataGridViewImageColumn();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tlUsuarios.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocentes)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlUsuarios
+            // 
+            this.tlUsuarios.ColumnCount = 2;
+            this.tlUsuarios.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.30206F));
+            this.tlUsuarios.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.69794F));
+            this.tlUsuarios.Controls.Add(this.dgvDocentes, 0, 1);
+            this.tlUsuarios.Controls.Add(this.panel1, 0, 0);
+            this.tlUsuarios.Controls.Add(this.panel2, 1, 2);
+            this.tlUsuarios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlUsuarios.Location = new System.Drawing.Point(0, 0);
+            this.tlUsuarios.Name = "tlUsuarios";
+            this.tlUsuarios.RowCount = 2;
+            this.tlUsuarios.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tlUsuarios.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlUsuarios.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tlUsuarios.Size = new System.Drawing.Size(559, 312);
+            this.tlUsuarios.TabIndex = 13;
+            // 
+            // dgvDocentes
+            // 
+            this.dgvDocentes.AllowUserToAddRows = false;
+            this.dgvDocentes.AllowUserToDeleteRows = false;
+            this.dgvDocentes.AllowUserToResizeColumns = false;
+            this.dgvDocentes.AllowUserToResizeRows = false;
+            this.dgvDocentes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDocentes.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvDocentes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocentes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Apellido,
+            this.Quitar});
+            this.tlUsuarios.SetColumnSpan(this.dgvDocentes, 2);
+            this.dgvDocentes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDocentes.Location = new System.Drawing.Point(3, 38);
+            this.dgvDocentes.MultiSelect = false;
+            this.dgvDocentes.Name = "dgvDocentes";
+            this.dgvDocentes.ReadOnly = true;
+            this.dgvDocentes.RowHeadersVisible = false;
+            this.dgvDocentes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDocentes.Size = new System.Drawing.Size(553, 243);
+            this.dgvDocentes.TabIndex = 3;
+            this.dgvDocentes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDocentes_CellClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnAgregar);
+            this.panel1.Controls.Add(this.cmbDocentes);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(459, 29);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(286, 1);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // cmbDocentes
+            // 
+            this.cmbDocentes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDocentes.FormattingEnabled = true;
+            this.cmbDocentes.Location = new System.Drawing.Point(68, 2);
+            this.cmbDocentes.Name = "cmbDocentes";
+            this.cmbDocentes.Size = new System.Drawing.Size(202, 21);
+            this.cmbDocentes.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Docentes:";
+            // 
+            // Nombre
+            // 
+            this.Nombre.DataPropertyName = "nombre";
+            this.Nombre.FillWeight = 130.9645F;
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // Apellido
+            // 
+            this.Apellido.DataPropertyName = "apellido";
+            this.Apellido.FillWeight = 130.9645F;
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.Name = "Apellido";
+            this.Apellido.ReadOnly = true;
+            // 
+            // Quitar
+            // 
+            this.Quitar.FillWeight = 38.07107F;
+            this.Quitar.HeaderText = "Quitar";
+            this.Quitar.Image = global::UI.Desktop.Properties.Resources.delete;
+            this.Quitar.Name = "Quitar";
+            this.Quitar.ReadOnly = true;
+            this.Quitar.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, -1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Salir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Location = new System.Drawing.Point(468, 287);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(88, 25);
+            this.panel2.TabIndex = 5;
+            // 
+            // DocentesCursos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 312);
+            this.Controls.Add(this.tlUsuarios);
+            this.Name = "DocentesCursos";
+            this.Text = "DocentesCursos";
+            this.Load += new System.EventHandler(this.DocentesCursos_Load);
+            this.tlUsuarios.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocentes)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlUsuarios;
+        private System.Windows.Forms.DataGridView dgvDocentes;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ComboBox cmbDocentes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewImageColumn Quitar;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button4;
+
+    }
+}
---
+++ TP2/UI.Desktop/DocentesCursos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Desktop
+{
+    public partial class DocentesCursos : ApplicationForm
+    {
+        private Curso cursoActual = null;
+        public DocentesCursos()
+        {
+            InitializeComponent();
+           
+        }
+        public DocentesCursos(Curso cur):this()
+        {
+            this.cursoActual = cur;
+            this.Listar();
+        }
+
+        private void DocentesCursos_Load(object sender, EventArgs e)
+        {
+           UsuarioLogic ul = new UsuarioLogic();
+           
+            this.cmbDocentes.DataSource =  ul.GetAll(TiposUsuarios.Docente);
+            this.cmbDocentes.DisplayMember = "NombreCompleto";
+            this.cmbDocentes.ValueMember = "ID";
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            if (!ul.isAssignedDocenteToCurso((int)this.cmbDocentes.SelectedValue, (int)this.cursoActual.ID))
+            {
+                ul.addDocenteToCurso((int)this.cmbDocentes.SelectedValue, (int)this.cursoActual.ID);
+            }
+            else {
+                MessageBox.Show("El docente ya esta asignado");
+            }
+
+           
+            this.Listar();
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Listar();
+        }
+
+        public void Listar()
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            this.dgvDocentes.AutoGenerateColumns = false;
+            this.dgvDocentes.DataSource = ul.GetDocentesByCurso(this.cursoActual.ID);
+        }
+
+        private void dgvDocentes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+             DataGridView dg = (DataGridView)sender;
+             Usuario cur = (Usuario)dg.CurrentRow.DataBoundItem;
+            if (e.ColumnIndex == 2)
+            {
+                if (DialogResult.Yes == MessageBox.Show("Desea quitar el docente del curso", "Quitar el docente del curso?", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)) {
+                     UsuarioLogic ul = new UsuarioLogic();
+                     ul.removeDocenteToCurso(cur.ID, this.cursoActual.ID);
+                     this.Listar();
+                }
+            }
+        }
+    }
+}
